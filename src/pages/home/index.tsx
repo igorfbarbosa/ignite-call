9f6706282f7b369fad2ad2e1b9a318da7fe5189b
igorfbarbosa <igorfbarbosa@gmail.com>
2024-04-27 16:54:34 -0300

    Cadastro de usuário - Estrutura visual da home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,29 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1">Agendamento descomplicado</Heading>
+        <Text size="lg">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando a aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
