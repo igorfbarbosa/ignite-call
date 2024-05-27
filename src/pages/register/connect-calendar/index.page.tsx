58be636f4eb6afb5535d73c29597232c0994b0a5
igorfbarbosa <igorfbarbosa@gmail.com>
2024-05-27 11:18:31 -0300

    Conexão com calendário - Configurando o Next Auth

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -3,8 +3,11 @@ import { ArrowRight } from 'phosphor-react'
 // import { api } from '@/lib/axios'
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn, useSession } from 'next-auth/react'
 
 export default function Register() {
+  const session = useSession()
+
   // async function handleRegister() {
 
   // }
@@ -24,11 +27,18 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight />
           </Button>
         </ConnectItem>
+
+        <Text>{JSON.stringify(session.data)}</Text>
+
         <Button type="submit">
           Próximo passo
           <ArrowRight />
